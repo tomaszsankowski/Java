64688fed10f21653f627cb8836da711e2fe82d64
tomaszsankowski <sankowski.tomek@gmail.com>
2024-04-02 00:07:30 +0200

    test autologin

--- lab5 - JUnit and Mockito/src/main/java/org/example/Main.java
+++ lab5 - JUnit and Mockito/src/main/java/org/example/Main.java
@@ -7,14 +7,17 @@ public class Main {
         System.out.println(mageController.save("Gandalf", 100));
         System.out.println(mageController.save("Saruman", 99));
         System.out.println(mageController.save("Radagast", 98));
+        System.out.println(mageController.save("", -2));
         System.out.println(mageController.save("Gandalf", 3));
         System.out.println(mageController.find("Gandalf"));
         System.out.println(mageController.find("Saruman"));
         System.out.println(mageController.find("Radagast"));
+        System.out.println(mageController.find(""));
         System.out.println(mageController.find("Sauron"));
         System.out.println(mageController.delete("Gandalf"));
         System.out.println(mageController.delete("Saruman"));
         System.out.println(mageController.delete("Radagast"));
+        System.out.println(mageController.delete(""));
         System.out.println(mageController.delete("Sauron"));
     }
 }
